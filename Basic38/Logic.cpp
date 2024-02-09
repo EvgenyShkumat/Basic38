ed72647220745498f4a4a7059c034f32543492b3
Евгений Шкумат <kotbmasle@gmail.com>
2024-02-09 19:53:37 +0300

    add is_matix_triangular_upper and is_matix_triangular_lower functions

---
+++ Basic38/Logic.cpp
@@ -0,0 +1,35 @@
+#define N 4
+
+bool is_matix_triangular_upper(int matrix[N][N]) {
+	bool triangle = true;
+
+	for (int i = 1; i < N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (matrix[i][j] != 0) {
+				triangle = false;
+				break;
+			}
+		}
+	}
+
+	return triangle;
+}
+
+bool is_matix_triangular_lower(int matrix[N][N]) {
+	bool triangle = true;
+
+	for (int i = N - 1; i > 0; i--)
+	{
+		for (int j = i; j > 0; j--)
+		{
+			if (matrix[i][j] != 0) {
+				triangle = false;
+				break;
+			}
+		}
+	}
+
+	return triangle;
+}
--- Basic38/Main.cpp
+++ Basic38/Main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#define N 5
+#define N 4
 using namespace std;
+
+void random_matrix_init(int matrix[N][N], int min, int max);
+string convert_matrix_to_string(int matrix[N][N]);
+
+bool is_matix_triangular_upper(int matrix[N][N]);
+bool is_matix_triangular_lower(int matrix[N][N]);
 
 int main() {
 	int matrix[N][N];
+
+	random_matrix_init(matrix, 0, 1);
+
+	cout << "Matrix:\n" << convert_matrix_to_string(matrix);
+
+	cout << (is_matix_triangular_upper(matrix) == 0 ? "No" : "Yes") << endl;
+	cout << (is_matix_triangular_upper(matrix) == 0 ? "No" : "Yes");
 
 	return 0;
 }
